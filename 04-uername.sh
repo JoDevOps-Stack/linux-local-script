f5a22840b3c9aa7357fa5985671f0f528a6fe690
Jyothi <jyothi.bandikatla0106@gmil.com>
2025-01-06 22:08:11 +0530

    conversation changed

--- 04-uername.sh
+++ 04-uername.sh
@@ -1,3 +1,6 @@
 #!bin/bash
-echo "enter your USERNAME"
-read "username"
+echo "enter your USERNAME" #TEXT ENTERED HERE WILL BE ADDED AS VALUE TO VARIABLE
+
+read "username" # HERE  USERNAME is the variable name
+
+echo "username entered: $USERNAME"

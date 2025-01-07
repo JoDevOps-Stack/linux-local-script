8912bd689e531fbec96d61d9dff7c0e6a080fb8a
Jyothi <jyothi.bandikatla0106@gmil.com>
2025-01-07 16:30:49 +0530

    conversation changed

--- 09-install-script.sh
+++ 09-install-script.sh
@@ -8,4 +8,20 @@ then
 fi
 
 dnf install mysql -y
-dng install git -y
+
+if [ $? -ne 0 ]
+  then
+      echo " installing mysql failure"
+  exit 1
+  else
+      echo "installing mysql success..."
+      dnf install git -y
+      if [ $? ne 0 ]
+      then
+         echo "installing git failure.."
+         exit 1
+   else
+   echo "installing git successs...."
+   
+   fi
+         

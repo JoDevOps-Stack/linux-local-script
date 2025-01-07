cd4f02dc48dd9441039daf01c7a7c0ac64179555
Jyothi <jyothi.bandikatla0106@gmil.com>
2025-01-07 13:19:03 +0530

    conversation changed

--- 05-data-type.sh
+++ 05-data-type.sh
@@ -5,4 +5,4 @@ NUMBER2=$2
 SUM=$((NUMBER1+NUMBER2))
 TIMESTAMP=$(DATE)
 echo "script executed at: $TIMESTAMP"
-ech "sum of $NUMBER1 AND $NUMBER2 IS :$SUM"
+ech "sum of $NUMBER1 AND $NUMBER2 IS :$SUM"05

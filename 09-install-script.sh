0eda2a1c1e1351ad33024657560deaf2adea7419
Jyothi <jyothi.bandikatla0106@gmil.com>
2025-01-07 16:07:46 +0530

    conversation changed

--- 09-install-script.sh
+++ 09-install-script.sh
@@ -4,10 +4,6 @@ USERID=$(id -u)
 if [$USERID ne 0]
 then
      echo "ERROR: you must have sudo access to execute this script"
+fi
 
-else
-
-if [$USERID eq 0]
-then 
-    echo "you can continue"
- fi
+dnf install mysql -y

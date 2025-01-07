678eebb629811db95096d732f2633a03fa8244ae
Jyothi <jyothi.bandikatla0106@gmil.com>
2025-01-07 15:13:09 +0530

    conversation changed

--- 07-special-variables.sh
+++ 07-special-variables.sh
@@ -5,7 +5,7 @@ echo "number of variables:  $#"
 echo "script name: $0"
 echo "present working directory: $PWD"
 echo "home directory of current user: $HOME"
-echo "which user is running script: $USERr"
+echo "which user is running script: $USER"
 echo "process id of current  script: $$"
 sleep 60 &
 echo "process id of last command in background : $!"

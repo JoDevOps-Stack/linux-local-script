dcb4a6ffca856d378fb7b33a9ac4c46e738d67b2
Jyothi <jyothi.bandikatla0106@gmil.com>
2025-01-08 12:46:59 +0530

    conversation changed

--- 12-logs.sh
+++ 12-logs.sh
@@ -5,9 +5,9 @@ G="\e[32m"
 Y="\e[33m"
 
 LOGS_FOLDER="/var/log/shellscript-logs"
-LOG_FILE= $( echo $0 | cut -d "." -f1 )
+LOG_FILE= $(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE/$TIMESTAMP.log"
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
@@ -20,7 +20,7 @@ VALIDATE(){
       fi
 }
 
-echo "script started executing at: $TIMESTAMP" &>>$LOG_FILE
+echo "script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
 if [ $USERID -ne 0 ]
 then
@@ -28,11 +28,11 @@ then
      exit 1
 fi
 
- dnf  list installed mysql &>>$LOG_FILE
+ dnf  list installed mysql &>>$LOG_FILE_NAME
 
   if [ $? -ne 0 ]
   then # not installed
-    dnf install mysql -y &>>$LOG_FILE
+    dnf install mysql -y &>>$LOG_FILE_NAME
     VALIDATE $? "installing mysql"
    if [ $? -ne 0 ]
   then
@@ -45,10 +45,10 @@ fi
   else
       echo  -e " mysql is already  $Y installed"
    fi
-dnf  list installed git &>>$LOG_FILE
+dnf  list installed git &>>$LOG_FILE_NAME
      if [ $? -ne 0 ]
      then
-      dnf install git -y &>>$LOG_FILE
+      dnf install git -y &>>$LOG_FILE_NAME
       VALIDATE $? "installing git"
       if [ $? -ne 0 ]
       then

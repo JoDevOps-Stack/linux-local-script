7b6df538e537e1d0f3cba0dc689ce338e87d8d77
Jyothi <jyothi.bandikatla0106@gmil.com>
2025-01-15 22:08:48 +0530

    conversation changed

--- 15-delete-oldlogs.sh
+++ 15-delete-oldlogs.sh
@@ -37,3 +37,11 @@ echo "script started executing at: $TIMESTAMP"  &>>$LOG_FILE_NAME
 
 FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)  # "to find files to be deleted"
 echo "files to be deleted: $FILES_TO_DELETE"
+
+while read -r file
+
+do
+     echo "deleting file: $file"
+
+done <<< $FILES_TO_DELETE
+
--- 16-while-loop.sh
+++ 16-while-loop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+# while loop is used to read the file
 while read -r line
 do 
 echo $line 
